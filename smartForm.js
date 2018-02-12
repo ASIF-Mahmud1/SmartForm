3e4407fb0617fb172375cc55fc504dad874503c0
Rizuwan <1430814@iub.edu.bd>
2018-02-12 09:27:56 +0600

    Practice

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,48 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+
+name :'',
+email : '',
+html : {likes :[], dislikes:[]},
+css : {likes :[], dislikes:[]} ,
+js : {likes :[], dislikes:[]} ,
+strength: {css: '', js: '', html: ''} ,
+currentQuestion : '#welcome'
+
+
+
+
+
+
+};
+
+$('#start').click(function()
+{
+  console.log("start");
+  $('#welcome').hide();
+  $('#q1').show();
+});
+
+$('name').change(function(event)
+{
+  //console.log(event);
+  console.log($('#name').val());
+});
+
+$('#next').click(function()
+{
+  if($('#name').val() && $('#email').val())
+  {
+  userData.name= $('name').val();
+  userData.email= $('email').val();
+  $('#welcome').hide();
+  $('#q1').hide();
+  $('#q2').show();
+}
+
+else
+{
+  alert('Please Enter Name & email');
+}
+});
